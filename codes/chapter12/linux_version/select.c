7fd3fc3f83b24b051b9d7b8c30a6a1c0fec9b052
cxcacac <cxcongyeah@126.com>
2022-05-21 22:23:58 +0800

    add to chapter 16 codes

---
+++ codes/chapter12/linux_version/select.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+#define BUF_SIZE 30
+
+int main(int argc, char* argv[]){
+    fd_set reads, tmps;
+    int result, str_len;
+    char buf[BUF_SIZE];
+    struct timeval timeout;
+    
+    // FDZERO(fd_set *fdset);
+    // FD_SET(int fd, fd_set* fdset);
+    // FD_CLR(int fd, fd_set* fdset);
+    // FD_ISSET(int fd, fd_set* fdset);
+
+    FD_ZERO(&read);
+    // 0 is standard input(console)
+    FD_SET(0, &read);
+
+    // timeout.tv_sec = 5;
+    // timeout.tv_usec = 5000;
+
+    while(1){
+        tmps = reads;
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 0;
+        result = select(1, &tmps, 0, 0, &timeout);
+        if(result==-1){
+            puts("select() error!");
+        }
+        else if(result==0){
+            puts("Time-out!");
+        }
+        else{
+            if(FD_ISSET(0, &tmps)){
+                str_len = read(0, buf, BUF_SIZE);
+                buf[str_len] = 0;
+                printf("message from console: %s \n", buf);
+            }
+        }
+    }
+    return 0;
+}
---
+++ codes/chapter15/desto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void){
+    FILE *fp;
+    int fd = open("data.dat", O_WRONLY | O_CREAT| O_TRUNC);
+    if(fd==-1){
+        fputs("file open error", stdout);
+        return -1;
+    }
+    // FILE * fdopen(int filedes, const char* mode)
+    fp = fdopen(fd, "w");
+    fputs("Network C programming \n", fp);
+    fclose(fp);
+    return 0;
+}
